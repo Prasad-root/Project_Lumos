ae2a116c51ef7f9952cd74938cffbe203c626794
Prasad Nirmal <113760595+Prasad-root@users.noreply.github.com>
2025-01-02 04:04:20 -0800

    done

--- templates/js/main.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-    "use strict";
-    // Sidebar Toggler
-    $('.sidebar-toggler').click(function () {
-        $('.sidebar, .content').toggleClass("open");
-        return false;
-    });
-})(jQuery);
-
